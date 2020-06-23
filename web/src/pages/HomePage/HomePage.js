dfb0eb62b67de5cccbeecaa724a19c58e6a17fde
Zack McKenna <zackmckenna@gmail.com>
2020-06-23 15:27:46 -0400

    added about page

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div>
-      <h1>HomePage</h1>
+      <h1>Mintbean Hackathon 6-22</h1>
       <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
     </div>
   )
