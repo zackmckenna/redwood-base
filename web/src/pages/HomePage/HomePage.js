a046b8ce33a3c62c11753671c21d7fdc60cab3d5
Zack McKenna <zackmckenna@gmail.com>
2020-06-23 16:42:39 -0400

    added toggleable menu

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,9 +1,13 @@
+import BasicLayout from 'src/layouts/BasicLayout'
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>Mintbean Hackathon 6-22</h1>
-      <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
-    </div>
+    <BasicLayout>
+      <div>
+        <h1>Mintbean Hackathon 6-22</h1>
+        <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
+      </div>
+    </BasicLayout>
   )
 }
 
